f078c995df6c5d33834486d98dd62317a49a4143
spencer741 <sa.business@outlook.com>
2025-01-19 11:26:16 -0600

    Added in autovalidate that didn't get committed last time

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include "../../examples/autotest-validate/autotest-validate.h"
 #include "../../assignment-autotest/test/assignment1/username-from-conf-file.h"
-
 /**
 * This function should:
 *   1) Call the my_username() function in autotest-validate.c to get your hard coded username.
@@ -17,6 +16,13 @@ void test_validate_my_username()
     /**
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
-     */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+   */
+
+    const char* conf_file_user_name = malloc_username_from_conf_file();
+
+    const char* hardcoded_user_name = my_username();
+     
+    TEST_ASSERT_EQUAL_STRING(conf_file_user_name,hardcoded_user_name);
+
+
 }
